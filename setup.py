923787e35dd7eb8a5557198a42b8c214b3b672e9
ashishsingh0098 <ashishsingh217070@gmail.com>
2024-06-01 07:29:43 +0530

    update setup logger utils

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from typing import List
 
 HYPHEN_E_DOT = '-e .'
 
+
 def get_requirements(file_path: str) -> List[str]:
-    'This function read the requirements files and return a list of requi;red lib'
+    """This function reads the requirements file and returns a list of required libraries."""
 
     with open(file_path, 'r') as file_obj:
         requirements = [req.strip() for req in file_obj.readlines()]
@@ -16,10 +17,10 @@ def get_requirements(file_path: str) -> List[str]:
 
 
 setup(
-    name = 'Steam-store-data-ML-project',
-    version = '1.0.0',
-    author = 'Ashish Singh',
-    author_email = 'ashishsingh217070@gmail.com',
-    packages = find_packages(),
-    install_requires = get_requirements('get_requirements.txt')
-    )
+    name='Steam-store-data-ML-project',
+    version='1.0.0',
+    author='Ashish Singh',
+    author_email='ashishsingh217070@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
---
+++ src/utils.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+from src.exception import CustomException
+
